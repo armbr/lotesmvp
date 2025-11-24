bb0d02b573762ab607e1599dba047a1b9a076c1c
Anderson <anderson@loteadora.local>
2025-11-24 16:31:58 -0300

    fix: add LatLngExpression type to MapContainer center prop

--- components/LoteMap.tsx
+++ components/LoteMap.tsx
@@ -1,13 +1,15 @@
 'use client'
 import { MapContainer as RLMapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import type { LatLngExpression } from 'leaflet'
 import React from 'react'
 import 'leaflet/dist/leaflet.css'
 
 export default function LoteMap({ features }: any) {
+  const center: LatLngExpression = [-23.5, -46.6]
+  
   return (
     <div className="h-80 rounded overflow-hidden">
-      {/* Cast to any to avoid strict MapContainer prop typings during build */}
-      <RLMapContainer center={[-23.5, -46.6]} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <RLMapContainer center={center} zoom={13} style={{ height: '100%', width: '100%' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />   
         {features && (() => {
           const AnyGeoJSON = GeoJSON as unknown as React.ComponentType<any>
